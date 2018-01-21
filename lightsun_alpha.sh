d4a7214f64dacafad4b36653edef34a6bf90ae6d
pook <pook@pook-iMac.local>
2018-01-22 02:26:08 +0900

    Version 2 (alpha)

--- lightsun_alpha.sh
+++ lightsun_alpha.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 # lightsun
-TOOL_VERSION=1
+TOOL_VERSION=2
 TOOL_BUILD=alpha
 SEEDUTIL_COMMAND="sudo /System/Library/PrivateFrameworks/Seeding.framework/Versions/A/Resources/seedutil"
+SYSTEM_BUILD="$(sw_vers -buildVersion)"
+SYSTEM_VERSION="$(sw_vers -productVersion)"
+
+function setDefaultSettings(){
+	PLATFORM=macOS
+	detectCatalog
+}
 
 function setProjectPath(){
 	COUNT=1
@@ -23,45 +30,45 @@ function showInferface(){
 	while(true); do
 		clear
 		showLines "*"
-		echo "lightsun (Version: ${TOOL_VERSION}) by pookjw"
+		echo "lightsun_${TOOL_BUILD}-${TOOL_VERSION} by pookjw"
 		showLines "-"
 		if [[ -z "${PLATFORM}" ]]; then
-			echo "(1) Platform: (undefined)"
+			echo "(1) Platform (required): (undefined)"
 		else
-			echo "(1) Platform: ${PLATFORM}"
+			echo "(1) Platform (required): ${PLATFORM}"
+		fi
+		if [[ -z "${DEVICE}" ]]; then
+			echo "(2) Device: (undefined)"
+		else
+			echo "(2) Device: ${DEVICE}"
 		fi
 		if [[ -z "${VERSION}" ]]; then
-			echo "(2) Version: (undefined)"
+			echo "(3) Version: (undefined)"
 		else
-			echo "(2) Version: ${VERSION}"
+			echo "(3) Version: ${VERSION}"
 		fi
 		if [[ -z "${BUILD}" ]]; then
-			echo "(3) Build: (undefined)"
+			echo "(4) Build: (undefined)"
 		else
-			echo "(3) Build: ${BUILD}"
+			echo "(4) Build: ${BUILD}"
 		fi
 		if [[ -z "${CATALOG}" ]]; then
-			echo "(4) Catalog: (undefined)"
+			echo "(5) Catalog (required): (undefined)"
 		else
-			echo "(4) Catalog: ${CATALOG}"
+			echo "(5) Catalog (required): ${CATALOG}"
 		fi
 		if [[ -z "${PREREQUISITE_VERISON}" ]]; then
-			echo "(5) Version (Prerequisite): (undefined)"
+			echo "(6) Version (Prerequisite): (undefined)"
 		else
-			echo "(5) Version (Prerequisite): ${PREREQUISITE_VERISON}"
+			echo "(6) Version (Prerequisite): ${PREREQUISITE_VERISON}"
 		fi
-		if [[ ! "${PLATFORM}" == macOS ]]; then
-			if [[ -z "${PREREQUISITE_BUILD}" ]]; then
-				echo "(6) Build (Prerequisite): (undefined)"
-			else
-				echo "(6) Build (Prerequisite): ${PREREQUISITE_BUILD}"
-			fi
-			showLines "-"
-			echo "commands: 1~6, adv, back, exit, start"
+		if [[ -z "${PREREQUISITE_BUILD}" ]]; then
+			echo "(7) Build (Prerequisite): (undefined)"
 		else
-			showLines "-"
-			echo "commands: 1~5, adv, back, exit, start"
+			echo "(7) Build (Prerequisite): ${PREREQUISITE_BUILD}"
 		fi
+		showLines "-"
+		echo "commands: 1~7, adv, back, exit, reset, start"
 		showLines "*"
 		readAnswer
 
@@ -73,17 +80,25 @@ function showInferface(){
 				showTitleBar
 				showLines "-"
 				echo "(1) macOS"
-				echo "(2) The others (iOS, tvOS, etc...)"
+				echo "(2) iOS"
 				showLines "*"
 				readAnswer
 
 				if [[ "${ANSWER}" == 1 ]]; then
+					resetValues
 					PLATFORM=macOS
 					CATALOG=
+					echo "Fill Version and Build with current macOS? (y/n)"
+					readAnswer
+					if [[ "${ANSWER}" == y || "${ANSWER}" == yes ]]; then
+						VERSION="${SYSTEM_VERSION}"
+						BUILD="${SYSTEM_BUILD}"
+					fi
 					backTitleBar
 					break
 				elif [[ "${ANSWER}" == 2 ]]; then
-					PLATFORM=TheOthers
+					resetValues
+					PLATFORM=iOS
 					CATALOG=
 					backTitleBar
 					break
@@ -92,10 +107,12 @@ function showInferface(){
 				fi
 			done
 		elif [[ "${ANSWER}" == 2 ]]; then
-			readAnswer "VERSION=" VERSION
+			readAnswer "DEVICE=" DEVICE
 		elif [[ "${ANSWER}" == 3 ]]; then
-			readAnswer "BUILD=" BUILD
+			readAnswer "VERSION=" VERSION
 		elif [[ "${ANSWER}" == 4 ]]; then
+			readAnswer "BUILD=" BUILD
+		elif [[ "${ANSWER}" == 5 ]]; then
 			addTitleBar "Set catalog"
 			if [[ -z "${PLATFORM}" ]]; then
 				showError "Define Platform first."
@@ -135,16 +152,19 @@ function showInferface(){
 					fi
 				done
 			fi
-		elif [[ "${ANSWER}" == 5 ]]; then
-			readAnswer "PREREQUISITE_VERISON=" PREREQUISITE_VERISON
 		elif [[ "${ANSWER}" == 6 ]]; then
-			if [[ "${PLATFORM}" == macOS ]]; then
-				showNotSupportedCommand
+			readAnswer "PREREQUISITE_VERISON=" PREREQUISITE_VERISON
+		elif [[ "${ANSWER}" == 7 ]]; then
+			readAnswer "PREREQUISITE_BUILD=" PREREQUISITE_BUILD
+		elif [[ "${ANSWER}" == reset ]]; then
+			resetValues
+		elif [[ "${ANSWER}" == start ]]; then
+			if [[ -z "${PLATFORM}" || -z "${CATALOG}" ]]; then
+				showError "Fill Platform and Catalog."
+				showPA2C
 			else
-				readAnswer "PREREQUISITE_BUILD=" PREREQUISITE_BUILD
+				startService
 			fi
-		elif [[ "${ANSWER}" == adv ]]; then
-			showAdvancedSettings
 		else
 			replyAnswer
 		fi
@@ -152,6 +172,7 @@ function showInferface(){
 }
 
 function detectCatalog(){
+	CATALOG=
 	if [[ "${PLATFORM}" == macOS ]]; then
 		CURRENT_ENROLLED_SEED=$(${SEEDUTIL_COMMAND} current | grep "Currently enrolled in" | cut -d" " -f4)
 		echo "Currently enrolled in: ${CURRENT_ENROLLED_SEED}"
@@ -163,10 +184,39 @@ function detectCatalog(){
 		if [[ "${CURRENT_ENROLLED_SEED}" == "(null)" ]]; then
 			${SEEDUTIL_COMMAND} unenroll > /dev/null 2>&1
 		fi
+		showPA2C
 	else
-		:
+		readAnswer "Path of .mobileconfig: " PATH_MOBILECONFIG
+		if [[ -z "${PATH_MOBILECONFIG}" ]]; then
+			:
+		elif [[ ! -f "${PATH_MOBILECONFIG}" ]]; then
+			showError "No such file: ${PATH_MOBILECONFIG}"
+		elif [[ -d "${PATH_MOBILECONFIG}" ]]; then
+			showError "${PATH_MOBILECONFIG} is directory."
+		else
+			VALUE=
+			PASS_ONCE_1=No
+			for VALUE in $(strings "${PATH_MOBILECONFIG}"); do
+				if [[ "${VALUE}" == "<key>MobileAssetServerURL-com.apple.MobileAsset.SoftwareUpdate</key>" ]]; then
+					PASS_ONCE_1=YES
+				elif [[ "${PASS_ONCE_1}" == YES ]]; then
+					CATALOG="$(echo "${VALUE}" | cut -d">" -f2 | cut -d"<" -f1)"
+					PASS_ONCE_1=NO
+					break
+				fi
+			done
+		fi
 	fi
-	showPA2C
+}
+
+function resetValues(){
+	PLATFORM=
+	DEVICE=
+	VERSION=
+	BUILD=
+	CATALOG=
+	PREREQUISITE_VERISON=
+	PREREQUISITE_BUILD=
 }
 
 function readAnswer(){
@@ -293,6 +343,77 @@ function backTitleBar(){
 	TITLE_NUM=$((${TITLE_NUM}-1))
 }
 
+function startService(){
+	downloadCatalog
+	parseAssets
+	quitTool 0
+}
+
+function downloadCatalog(){
+	deleteFile "${PROJECT_DIR}/assets.gz"
+	deleteFile "${PROJECT_DIR}/assets"
+	deleteFile "${PROJECT_DIR}/assets.xml"
+	deleteFile "${PROJECT_DIR}/documentation.xml"
+	if [[ "${PLATFORM}" == macOS ]]; then
+		curl -# -o "${PROJECT_DIR}/assets.gz" "${CATALOG}"
+		gunzip "${PROJECT_DIR}/assets.gz"
+		if [[ ! -f  "${PROJECT_DIR}/assets" ]]; then
+			showError "Failed to get assets."
+			quitTool 1
+		fi
+		mv "${PROJECT_DIR}/assets" "${PROJECT_DIR}/assets.xml"
+	else
+		curl -# -o "${PROJECT_DIR}/assets.xml" "${CATALOG}/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml"
+		curl -# -o "${PROJECT_DIR}/documentation.xml" "${CATALOG}/com_apple_MobileAsset_SoftwareUpdateDocumentation/com_apple_MobileAsset_SoftwareUpdateDocumentation.xml"
+		if [[ ! -f "${PROJECT_DIR}/assets.xml" || ! -f "${PROJECT_DIR}/documentation.xml" ]]; then
+			showError "Failed to get assets."
+			quitTool 1
+		fi
+	fi
+}
+
+function parseAssets(){
+	if [[ "${PLATFORM}" == macOS ]]; then
+		mkdir -p "${PROJECT_DIR}/data"
+		VALUE=
+		PASS_ONCE_1=YES
+		PASS_ONCE_2=NO
+		PASS_ONCE_3=YES
+		for VALUE in $(cat "${PROJECT_DIR}/assets.xml"); do
+			if [[ "${PASS_ONCE_1}" == YES ]]; then
+				SUB_VALUE_1="${VALUE}"
+				PASS_ONCE_1=NO
+				PASS_ONCE_2=YES
+				if [[ "${SUB_VALUE_1}" == "<key>ServerMetadataURL</key>" ]]; then
+					UPDATE_KEY="$(echo "${SUB_VALUE_2}" | cut -d">" -f2 | cut -d"<" -f1)"
+					START_RECORDING=YES
+				fi
+			elif [[ "${PASS_ONCE_2}" == YES ]]; then
+				SUB_VALUE_2="${VALUE}"
+				PASS_ONCE_2=NO
+				PASS_ONCE_3=YES
+				if [[ "${SUB_VALUE_2}" == "<key>ServerMetadataURL</key>" ]]; then
+					UPDATE_KEY="$(echo "${SUB_VALUE_3}" | cut -d">" -f2 | cut -d"<" -f1)"
+					START_RECORDING=YES
+				fi
+			elif [[ "${PASS_ONCE_3}" == YES ]]; then
+				SUB_VALUE_3="${VALUE}"
+				PASS_ONCE_3=NO
+				PASS_ONCE_1=YES
+				if [[ "${SUB_VALUE_3}" == "<key>ServerMetadataURL</key>" ]]; then
+					UPDATE_KEY="$(echo "${SUB_VALUE_1}" | cut -d">" -f2 | cut -d"<" -f1)"
+					START_RECORDING=YES
+				fi
+			fi
+			if [[ "${START_RECORDING}" == YES ]]; then
+				echo "${VALUE}" >> "${PROJECT_DIR}/data/${UPDATE_KEY}"
+			fi
+		done
+	else
+		:
+	fi		
+}
+
 function deleteFile(){
 	if [[ ! -z "${1}" ]]; then
 		if [[ -f "${1}" ]]; then
@@ -304,9 +425,10 @@ function deleteFile(){
 }
 
 function quitTool(){
-	deleteFile "${PROJECT_DIR}"
+	#deleteFile "${PROJECT_DIR}"
 	exit "${1}"
 }
 
+setDefaultSettings
 setProjectPath
 showInferface
